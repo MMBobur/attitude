d0e89e26f8f470f2cf3ab7aa49fc2310aa39c898
Your Name <you@example.com>
2021-08-22 16:27:30 +0500

    javodbek add page

---
+++ src/components/Cursus/Cursus.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Cursus.js';
+
+function Cursus() {
+  return (
+    <>
+    <div className="container">
+      <div className="block1">
+          <h2 className="text">Cursus penati saccum.</h2>
+          <p className="text-p">
+             Corem ipsum dolor sit amet consectetur adipisicing elit.
+              Fugiat deserunt repr reiciendis consectetur, aut, atusrepud
+             nandae voluptas atque mollitia dolorumtmollitia dolorumt umt 
+             dolorumt umt gert olorumt umt gert umt gert umt gerid
+             gert umt gert umt geri umt gert umt
+          </p>
+      </div>
+      <div className="block2">
+        <div className="box-1">
+            <div className="foo"> 
+                <h3 className="item">100</h3>
+                <p className="logo">x</p>
+                <h3 className="item">100</h3>
+            </div>
+        </div>
+        <div className="box-1">
+            <div className="foo"> 
+                <h3 className="item">100</h3>
+                <p className="logo">x</p>
+                <h3 className="item">100</h3>
+            </div>
+        </div>
+        <div className="box-1">
+          <div className="foo"> 
+                <h3 className="item">100</h3>
+                <p className="logo">x</p>
+                <h3 className="item">100</h3>
+          </div>
+        </div>
+        <div className="box-1">
+          <div className="foo"> 
+                <h3 className="item">100</h3>
+                <p className="logo">x</p>
+                <h3 className="item">100</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Cursus
